cbb82ed0d2c9b081f59d6e07cc9de9542a9685f3
Unknown <margarida.rpds@gmail.com>
2019-11-27 09:31:42 +0000

    [Add] ValueCard component and Homepage

---
+++ frontend/src/components/ValueCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 30,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const ValueCard = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card} style={{display: 'inline-block', margin: '2em'}} variant="outline">
+      <CardContent>
+        <Typography className={classes.title} gutterBottom >
+          {props.value}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {props.name}
+        </Typography>
+
+      </CardContent>
+      <CardActions>
+        <Button align="right" size="small">View {props.name} Details </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default ValueCard;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import MainPageLayout from './MainPageLayout';
+import ValueCard from '../components/ValueCard';
 
 const Homepage = () => {
+    
     return (
       <MainPageLayout>
-
+        <br/>
+        <br/>
+        <br/>
+        <ValueCard name="Purchases" value="123k"/>
+        <ValueCard name="Sales" value="343k"/>
+        <ValueCard name="Equity" value="53k"/>
+        <br/>
+        <ValueCard name="Purchases" value="123k"/>
+        <ValueCard name="Sales" value="343k"/>
+        <ValueCard name="Equity" value="53k"/>
+        <br/>
+        <ValueCard name="Purchases" value="123k"/>
+        <ValueCard name="Sales" value="343k"/>
+        <ValueCard name="Equity" value="53k"/>
       </MainPageLayout>
     );
   }
