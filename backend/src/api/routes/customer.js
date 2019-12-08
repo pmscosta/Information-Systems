555ed57a07a991a9172c741de3ff94c51058b257
FilipePintoReis <filipepintodosreis@gmail.com>
2019-12-08 22:08:39 +0000

    Customer route

---
+++ backend/src/api/routes/customer.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const router = require('express').Router();
+const customer = require('../../controllers/customer');
+
+router.get('/topclient', customer.getTopClients);
+
+module.exports = router;
