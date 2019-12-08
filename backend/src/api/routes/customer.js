e9079f62c7b5da34a5d1103bb6cd000c2ce7adaf
FilipePintoReis <filipepintodosreis@gmail.com>
2019-12-08 23:04:12 +0000

    added route for get all customers

--- backend/src/api/routes/customer.js
+++ backend/src/api/routes/customer.js
@@ -3,6 +3,8 @@
 const router = require('express').Router();
 const customer = require('../../controllers/customer');
 
+
+router.get('/', customer.getAll);
 router.get('/topclient', customer.getTopClients);
 
 module.exports = router;
