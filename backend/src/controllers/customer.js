8d35f3b40db0c256462ffe5b1b8e315067e94cf5
FilipePintoReis <filipepintodosreis@gmail.com>
2019-12-08 22:10:38 +0000

    added top client to customer controller

--- backend/src/controllers/customer.js
+++ backend/src/controllers/customer.js
@@ -1,11 +1,37 @@
 'use strict';
 
 const Customer = require('../models/customer');
+const Invoice = require('../models/invoice');
 
 function getAll(req, res) {
   Customer.find()
     .then(customer => res.json(customer))
     .catch(err => res.status(400).json(err));
+}
+
+async function getTopClients(req, res) {
+  let topCustomer = {
+    customer: null,
+    totalSpent: -Infinity
+  };
+
+  const customers = await Customer.find();
+
+  for(const customer of customers) {
+    const invoices = await Invoice.find({ customer: customer._id });
+    
+    let totalSpent = 0;
+
+    for(const i of invoices) {
+      totalSpent += i.netTotal;
+    }
+
+    if(topCustomer.totalSpent < totalSpent) {
+      topCustomer = { customer, totalSpent };
+    }
+  }
+
+  res.json(topCustomer);
 }
 
 function getByIdApi(req, res) {
@@ -20,5 +46,6 @@ function getById(id) {
 
 module.exports = {
   getAll,
-  getById
+  getById,
+  getTopClients
 };
