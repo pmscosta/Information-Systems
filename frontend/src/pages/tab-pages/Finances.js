668edac139ffe9892c5ad9a7345ded7717cd54d7
Margarida Silva <margarida.rpds@gmail.com>
2019-12-16 18:24:29 +0000

    Refactor Finances indicators update

--- frontend/src/pages/tab-pages/Finances.js
+++ frontend/src/pages/tab-pages/Finances.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { connect } from "react-redux";
 import React, { useEffect, useState } from "react";
 import ValueCard from "../../components/ValueCard";
 import { useSelector } from "react-redux";
@@ -7,6 +11,25 @@ import { Container } from "@material-ui/core";
 import SimpleValueCard from "../../components/SimpleValueCard";
 import AppLinearProgress from "../../components/AppLinearProgress";
 
+class Finances extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Accounts Payable: {this.props.accountsPayable}</div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    accountsPayable: state.purchases.invoiced
+      .filter(({ naturalKey }) => {
+        return !state.purchases.payments.find(x => x.sourceDoc === naturalKey);
+      })
+      .reduce((a, b) => a + b.payableAmount, 0)
+      .toFixed(2)
+  };
 const Finances = () => {
   const { loading: iloading, inventory } = useSelector(
     state => state.inventory
@@ -85,4 +108,6 @@ const Finances = () => {
   );
 };
 
-export default Finances;
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Finances);
